fb3101bfe2b0de5575c52d46361513877e7880ca
Jakub <jakubjetleb@gmail.com>
2022-04-19 15:51:41 +0200

    Theme tweak

--- cookies-consent.js
+++ cookies-consent.js
@@ -2,38 +2,25 @@
 window.dataLayer = window.dataLayer || [];
 
 window.CookieConsentTheme = {
-  'base-color': '#333',
-  'base-font-size': '15px',
-  'base-line': '1px solid #333',
-  'border-radius': '5px',
-  'button-border-radius': '5px',
+  'base-color': '#3c3c3c',
+  'border-radius': '0',
 
-  'color-grey': '#f2f4f9',
-  'color-primary': '#333',
-  'color-text': '#333',
-  'color-white': '#fff',
+  'color-primary': '#000000',
 
-  'button-default__bg-color': '#fff',
-  'button-default__color': '#333',
-  'button-default__text-transform': 'none',
-  'button-default__border': '1px solid #eee',
-  'button-default__box-shadow': 'none',
+  'button-default__bg-color': '#f3f3f2',
+  'button-default__color': '#676767',
+  'button-default__border': '0 none',
 
-  'button-default--hover__bg-color': '#fff',
-  'button-default--hover__color': '#333',
-  'button-default--hover__border': '1px solid #ccc',
-  'button-default--hover__box-shadow': '0 0 10px 0 rgba(0,0,0,0.05)',
+  'button-default--hover__bg-color': '#f3f3f2',
+  'button-default--hover__color': '#676767',
+  'button-default--hover__border': '0 none',
+  'button-default--hover__box-shadow': 'none',
 
-  'button-primary__bg-color': '#eee',
-  'button-primary__color': '#333',
-  'button-primary__text-transform': 'none',
-  'button-primary__border': 'none',
-  'button-primary__box-shadow': 'none',
+  'button-primary__bg-color': '#000000',
+  'button-primary__color': '#fff',
 
-  'button-primary--hover__bg-color': '#ccc',
-  'button-primary--hover__color': '#333',
-  'button-primary--hover__border': 'none',
-  'button-primary--hover__box-shadow': 'none',
+  'button-primary--hover__bg-color': '#f3f3f2',
+  'button-primary--hover__color': '#000',
 };
 
 (function cookiesInit() {
